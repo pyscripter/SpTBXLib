7a1875b1bad65d293db53ffce064b104a0b90001
pyscripter <pyscripter@gmail.com>
2021-09-13 15:18:45 +0300

    Synched with Silverpoint Added LIBSUFFIX AUTO to packages

--- Source/SpTBXItem.pas
+++ Source/SpTBXItem.pas
@@ -7905,6 +7905,7 @@ begin
   // Change the readonly IsToolbar property using RTTI, the property must
   // be published.
   // Tip from: http://hallvards.blogspot.com/2004/05/hack-1-write-access-to-read-only.html
+  // PBoolean(Integer(Self) + (Integer(GetPropInfo(TSpTBXPopupWindowView, 'IsToolbar').GetProc) and $00FFFFFF))^ := Value;
   PBoolean(@(Self.IsToolbar))^ := Value;
 end;
 
